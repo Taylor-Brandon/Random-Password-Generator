e9669210075f7e4ab252dae089b9ca59b3accb54
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-11-05 21:16:24 -0500

    added prompted questions for password customization

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
 // Assignment code here
 
+function promptQuestion() {
+    var criteria = window.confirm("Would you like to choose password criteria?");
+
+    if(!criteria) {
+        return;
+    }
+ var length = window.prompt("Set character length (min: 8 max: 128)");
+
+ if (length > 8 && length < 128) {
+    window.alert("You have choosen to use " + length + " characters")
+ } else {
+    window.alert("Please choose an approiate length!")
+    return;
+ }
+
+ var uppercase = window.confirm("Would you like any uppercase characters?");
+ if(uppercase) {
+    window.alert("You have choosen to use uppercase characters");
+ } else {
+    window.alert("You have choosen not to use uppercase characters");
+ }
+
+ var lowercase = window.confirm("Would you like any lowercase characters?");
+ if(lowercase) {
+    window.alert("You have choosen to use lowercase characters");
+ } else {
+    window.alert("You have choosen to not use lowercase characters");
+ }
+
+ var numeric = window.confirm("Would you like any numerical characters?");
+ if (numeric) {
+    window.alert("You have choosen to use numeric characters");
+ } else {
+    window.alert("You have choosen not to use numeric characters");
+ }
+
+ var specialCharacter = window.confirm("Are there any special characters that you would like to include?");
+ if (specialCharacter) {
+    window.alert("You have choosen to use special characters");
+ } else {
+    window.alert("You have choosen not use any special characters")
+ }
+
+ if (!uppercase && !lowercase && !numeric && ! specialCharacter) {
+    window.alert("You must choose at least one character type!")
+    return;
+ } else {
+    window.alert("Thank you!")
+ }
+
+
+
+
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +70,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", promptQuestion);
